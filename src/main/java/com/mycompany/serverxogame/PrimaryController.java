3b18cf3abb698b277db180d1a3a7e3e4b8d2b46e
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-12-20 13:36:56 +0200

    - trigger piechart with our DB

--- src/main/java/com/mycompany/serverxogame/PrimaryController.java
+++ src/main/java/com/mycompany/serverxogame/PrimaryController.java
@@ -1,6 +1,8 @@
 package com.mycompany.serverxogame;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -14,15 +16,20 @@ public class PrimaryController implements Initializable {
     @FXML
     private PieChart pieChart;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        PieChart.Data slice1 = new PieChart.Data("OnGame Players", 35);
-        PieChart.Data slice2 = new PieChart.Data("Available Players", 40);
-        PieChart.Data slice3 = new PieChart.Data("Offline Players", 25);
+   @Override
+public void initialize(URL url, ResourceBundle rb) {
+    try {
+        int[] counts = DAO.getPlayersStateCounts();
+        System.out.println(Arrays.toString(counts));
+
+
+        PieChart.Data slice1 = new PieChart.Data("OnGame Players", counts[0]);
+        PieChart.Data slice2 = new PieChart.Data("Available Players", counts[1]);
+        PieChart.Data slice3 = new PieChart.Data("Offline Players", counts[2]);
 
         pieChart.setData(FXCollections.observableArrayList(slice1, slice2, slice3));
 
-         Platform.runLater(() -> {
+        Platform.runLater(() -> {
             slice1.getNode().setStyle("-fx-pie-color: #FF6347; -fx-cursor: hand;");
             slice2.getNode().setStyle("-fx-pie-color: #90EE90; -fx-cursor: hand;");
             slice3.getNode().setStyle("-fx-pie-color: #1E90FF; -fx-cursor: hand;");
@@ -35,5 +42,9 @@ public class PrimaryController implements Initializable {
                 data.getNode().setOnMouseExited(e -> data.getNode().setScaleX(1.0));
             }
         });
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+}
+
 }
