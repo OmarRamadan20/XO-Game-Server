af28da5ead2f64cc57d69550be55e7ee51d30564
Omar <omarramadan1222@gmail.com>
2025-12-24 16:59:57 +0200

    Update : Server UI and Player Stat

--- src/main/java/com/mycompany/serverxogame/App.java
+++ src/main/java/com/mycompany/serverxogame/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/mycompany/serverxogame/server_run.fxml"));
         //F:\traning\iti\Java\Final Project\ServerXOGame\src\main\resources/com/mycompany/serverxogame/server_run.fxml
-        Scene scene = new Scene(root, 500, 500);
+        Scene scene = new Scene(root, 650, 650);
         primaryStage.setTitle("Server!!!!!!!!!!!!!!!!!");
         primaryStage.setScene(scene);
         primaryStage.show();
