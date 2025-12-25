767e2e130de82671abda6564571a3231b48af0cc
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-12-25 23:23:35 +0200

    - handle server stop

--- src/main/java/com/mycompany/serverxogame/App.java
+++ src/main/java/com/mycompany/serverxogame/App.java
@@ -2,6 +2,7 @@ package com.mycompany.serverxogame;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,8 +15,19 @@ public class App extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/com/mycompany/serverxogame/server_run.fxml"));
         //F:\traning\iti\Java\Final Project\ServerXOGame\src\main\resources/com/mycompany/serverxogame/server_run.fxml
         Scene scene = new Scene(root, 650, 650);
-        primaryStage.setTitle("Server!!!!!!!!!!!!!!!!!");
+        primaryStage.setTitle("My Server");
         primaryStage.setScene(scene);
+        
+         primaryStage.setOnCloseRequest(event -> {
+        System.out.println("Window closing...");
+        if (Server_runController.getServer() != null) {
+    Server_runController.getServer().stopServer();
+}
+        Platform.exit();
+        System.exit(0);
+    });
+
+ 
         primaryStage.show();
     }
 
@@ -23,4 +35,6 @@ public class App extends Application {
  
         launch(args);
      }
+    
+  
 }
